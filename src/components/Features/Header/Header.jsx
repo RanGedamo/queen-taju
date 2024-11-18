746270d98947ec00d9e9661ba40f1390a241a17b
RanGedamo <mcrangedamo@gmail.com>
2024-11-19 01:32:21 +0200

    taju

--- src/components/Features/Header/Header.jsx
+++ src/components/Features/Header/Header.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="header">
-      
-      <nav class="navbar">
-        <div class="navbar-logo">Beautify</div>
-        <ul class="navbar-links">
-            <li><a href="#home">המלצות לקוחות</a></li>
-            <li><a href="#about">עבודות</a></li>
-            <li><a href="#services">יצירת קשר</a></li>
+      <nav className="navbar">
+        <div className="navbar-logo">Beautify</div>
 
+        <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
+          <li><a href="#home">המלצות לקוחות</a></li>
+          <li><a href="#about">עבודות</a></li>
+          <li><a href="#services">יצירת קשר</a></li>
         </ul>
-    </nav>
-      {/* <div className="hamburger-menu">
-        <i className="fa fa-bars"></i>
-      </div> */}
+
+        {/* כפתור המבורגר */}
+        <button className="hamburger-button" onClick={toggleMenu}>
+          {isOpen ? '✖' : '☰'}
+        </button>
+      </nav>
     </header>
   );
 };
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -12,56 +12,33 @@ import tajuHome from "./Home-Img/taju-home.svg";
 function Home() {
   return (
     <div className="home">
-      {/* <nav class="navbar">
-        <div class="navbar-logo">Beautify</div>
-        <ul class="navbar-links">
-            <li><a href="#home">המלצות לקוחות</a></li>
-            <li><a href="#about">עבודות</a></li>
-            <li><a href="#services">יצירת קשר</a></li>
-
-        </ul>
-    </nav> */}
-
-      <div className="textHome d-flex">
-        <p>
-          {" "}
-          כל מה שאת צריכה זה קצת <br /> טגו{" "}
-        </p>
+      <div className="textHome">
+        <p>כל מה שאת צריכה זה קצת <br /> טגו</p>
       </div>
 
       <div className="flowerText">
         <img src={flower} alt="Flower" />
       </div>
 
-      <div className="ballwhite1">
-        <img src={ballwhite1} alt="Ball White 1" />
-      </div>
-      {/* <div className="ballwhite2">
-        <img src={ballwhite2} alt="Ball White 2" />
-      </div> */}
-
       <div className="makeupImgHome">
         <img src={makeupImgHome} alt="makeup Img Home" />
       </div>
 
       <div className="goldSphere">
-        <img src={goldSphere} alt="makeup Img Home" />
+        <img src={goldSphere} alt="Gold Sphere" />
       </div>
 
-      <div className="d-flex container-img">
-
-      <div className="Ellipse">
-        <img src={Ellipse} alt="makeup Img Home" />
+      <div className="container-img">
+        <div className="Ellipse">
+          <img src={Ellipse} alt="Ellipse" />
+        </div>
+        <div className="tajuHome">
+          <img src={tajuHome} alt="Taju Home" />
+        </div>
       </div>
-      <div className="tajuHome">
-        <img src={tajuHome} alt="makeup Img Home" />
-      </div>
-
-      </div>
-
-
     </div>
   );
 }
 
 export default Home;
+
